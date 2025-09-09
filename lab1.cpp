3d3d21563fa5a046e9c81bdfb06969711aa02041
qRugma <strugov.poma@yandex.ru>
2025-09-09 11:04:57 +0700

    start lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,28 @@
+#include "lib1.hpp"
+#include <iostream>
+#include <chrono>
+#include <random>
+
+int main(){
+
+    std::random_device randomDevice;
+    std::mt19937_64 generator(randomDevice());
+    int rangeStart, rangeEnd;
+
+    std::cin >> rangeStart >> rangeEnd;
+
+    std::uniform_int_distribution<int> distribution(rangeStart, rangeEnd);
+
+    std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
+    
+    for(int i=0; i < 20; i++){
+        std::cout << distribution(generator) << " ";
+    }
+    std::cout << std::endl;
+    
+    std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
+    
+    std::chrono::duration<double> duration = timeEnd - timeStart;
+
+    std::cout << duration.count() << std::endl;
+}
