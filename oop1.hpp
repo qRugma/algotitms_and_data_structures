6b86b62754f4dce1225c672494e5fd74d49a7961
qRugma <strugov.poma@yandex.ru>
2025-10-08 17:13:36 +0700

    resort definitions

--- oop1.hpp
+++ oop1.hpp
@@ -12,14 +12,30 @@ class FreeVector{
     FreeVector(){}
 
     //first three is start point, next is end point
-    FreeVector(int xS, int yS, int zS,
-               int xE, int yE, int zE);
+    FreeVector(double xS, double yS, double zS,
+               double xE, double yE, double zE);
 
     // size start and end need >= 3
-    FreeVector(int *Start, int *End);
+    FreeVector(double *Start, double *End);
     // FreeVector(const FreeVector&);
 
+
+    //first three is start point, next is end point
     // ~FreeVector(){}
+    void newCoors(double *Start, double *End);
+
+    // size start need >= 3
+    void newStart(const double* Start);
+    void newStart(double xS, double yS, double zS);
+
+    // size end need >= 3
+    void newEnd(const double* End);
+    void newEnd(double xE, double yE, double zE);
+
+    // size start and end need >= 3
+    void newCoors(const double* Start, const double* End);
+    
+    void print();
 
     const double* getStart();
 
@@ -27,20 +43,6 @@ class FreeVector{
     
     const double* getRadius();
 
-    void print();
-    //first three is start point, next is end point
-    void newCoors(int xS, int yS, int zS,
-                  int xE, int yE, int zE);
-
-    // size start need >= 3
-    void newStart(const int* Start);
-
-    // size end need >= 3
-    void newEnd(const int* End);
-
-    // size start and end need >= 3
-    void newCoors(const int* Start, const int* End);
-    
     double lenght() const;
 
     FreeVector orthonormal();
@@ -49,7 +51,7 @@ class FreeVector{
 
     FreeVector operator-(const FreeVector &rhs) const;
 
-    FreeVector operator*(const int num) const;
+    FreeVector operator*(const double num) const;
 
     bool operator==(const FreeVector &rhs) const;
     double scalar(const FreeVector &rhs) const;
