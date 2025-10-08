a4ee52b1f412b8f9c885134ba4df75c839a02ff1
qRugma <strugov.poma@yandex.ru>
2025-10-08 18:58:29 +0700

    add new handlers and remove old

--- oop1test.cpp
+++ oop1test.cpp
@@ -4,19 +4,10 @@
 void autotest(FreeVector vec, FreeVector vec2){
     FreeVector  vec3;
 
-    // vec.print();
-    // vec2.print();
-        
-    std::cout << "getStart: ";
-    for(int i=0; i<3; i++)
-        std::cout << vec.getStart()[i] << " ";
-    std::cout << std::endl << "getEnd: ";
-    for(int i=0; i<3; i++)
-        std::cout << vec.getEnd()[i] << " ";
-    std::cout << std::endl << "getRadius: ";
-    for(int i=0; i<3; i++)
-        std::cout << vec.getRadius()[i] << " ";
-    std::cout << std::endl;
+    std::cout << "vec: " << vec.getX() << " " << vec.getY() << " " << vec.getZ() << std::endl;
+
+    vec3.newCoors(1, 5, 2);
+    std::cout << "vec3: " << vec3 << std::endl;
 
     std::cout << "lenght=" << vec.lenght() << std::endl << std::endl;
     
@@ -63,7 +54,7 @@ void autotest(FreeVector vec, FreeVector vec2){
 
 
 int main(){
-    FreeVector vec{0, 1, 0, 1, 2, 3}, vec2{0, 2, 3, 0, -2, 6};
+    FreeVector vec{1, 1, 3}, vec2{1, -2, 6};
     int choise=0;
     std::cout << "0=standart, 1=input vectors: ";
     std::cin >> choise;
