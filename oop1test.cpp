ccddbab8a78d3e95a88f9eb14e75eecfdb5a6f61
qRugma <strugov.poma@yandex.ru>
2025-10-08 18:00:56 +0700

    rewrite with friends cout cin, add input vecs on start program

--- oop1test.cpp
+++ oop1test.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "oop1.hpp"
 
-void autotest(){
-        FreeVector vec{0, 1, 0, 1, 2, 3}, vec2{0, 2, 3, 0, -2, 6}, vec3;
+void autotest(FreeVector vec, FreeVector vec2){
+    FreeVector  vec3;
+
     // vec.print();
     // vec2.print();
-    // for(int i=0; i<3; i++)
-    //     vec.getRadius()[i] = 0;
         
-    //геттеры
     std::cout << "getStart: ";
     for(int i=0; i<3; i++)
         std::cout << vec.getStart()[i] << " ";
@@ -20,21 +18,23 @@ void autotest(){
         std::cout << vec.getRadius()[i] << " ";
     std::cout << std::endl;
 
-    std::cout << "lenght=" << vec.lenght() << std::endl;
+    std::cout << "lenght=" << vec.lenght() << std::endl << std::endl;
     
+    std::cout << "orthonormal: " << (vec3 = vec.orthonormal()).lenght() << std::endl;
+
     std::cout << "sum: " << std::endl;
     vec3 = vec + vec2;
-    vec3.print();
+    std::cout << vec3;
     std::cout << std::endl;
     
     std::cout << "subtraction: " << std::endl;
     vec3 = vec - vec2;
-    vec3.print();
+    std::cout << vec3;
     std::cout << std::endl;
 
     std::cout << "multipcation: " << std::endl;
     vec3 = vec*2;
-    vec3.print();
+    std::cout << vec3;
     std::cout << std::endl;
 
     std::cout << "equal: " << std::endl;
@@ -48,17 +48,29 @@ void autotest(){
 
     std::cout << "vector multiplication: " << std::endl;
     vec3 = vec.vectorProduct(vec2);
-    vec3.print();
+    std::cout << vec3;
     std::cout << std::endl;
 
     std::cout << "tripleProduct: " << std::endl;
     std::cout << vec.tripleProduct(vec2, vec3) << std::endl;
     std::cout << vec.tripleProduct(vec3, vec2) << std::endl;
+
+    std::cout << "doubleVectorProduct: " << std::endl;
+    vec3 = vec.doubleVectorProduct(vec2, vec3);
+    std::cout << vec3;
 }
 
 
 int main(){
+    FreeVector vec{0, 1, 0, 1, 2, 3}, vec2{0, 2, 3, 0, -2, 6};
     int choise=0;
+    std::cout << "0=standart, 1=input vectors: ";
+    std::cin >> choise;
+    std::cout << std::endl;
     if (choise == 0)
-        autotest();
+        autotest(vec, vec2);
+    if (choise == 1)
+        std::cin >> vec >> vec2;
+        autotest(vec, vec2);
+    
 }
