96eb3c39cb91ef3ba50f4979732912f7e32a5d4f
qRugma <strugov.poma@yandex.ru>
2025-10-14 08:54:40 +0700

    add vector header

---
+++ oop/2/vector.hpp
@@ -0,0 +1,50 @@
+class Vector {
+    int size_ = 0;
+    int capacity_ = 8;
+    int* data_ = nullptr;
+
+    
+    public:
+    Vector(int capacity=0);
+    
+    Vector(int* mas, int len);
+    
+    Vector(Vector &&vec);
+    
+    void resize(int size);
+    void resize(int size, int value);
+    int size() const;
+
+    void swap(Vector other);
+
+    int find(int elem) const;
+
+    friend std::ostream & operator << (std::ostream &cout, const Vector &vec);
+    friend std::istream & operator >> (std::istream &cin, Vector &vec);
+
+    void sort(bool reverse=false);
+
+    bool insert(const int index, const int value);
+    bool pop(const int index);
+    bool remove(const int value);
+    bool remove_all(const int value);
+    int max() const;
+    int min() const;
+
+    int* begin();
+    int* end();
+    const int* begin() const;
+    const int* end() const;
+
+    int& operator[](int index);
+    const int& operator[](int index) const;
+    Vector& operator=(const Vector &other);
+    Vector operator+(int value) const;
+    Vector& operator+=(int value);
+    Vector& operator=(Vector &&other);
+    Vector operator+(const Vector &other) const;
+    Vector& operator+=(const Vector &other);
+    bool operator==(const Vector &other) const;
+    bool operator!=(const Vector &other) const;
+
+};
