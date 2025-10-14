3c1b2fe39d2c2762863ddf66e2a540a8db049899
qRugma <strugov.poma@yandex.ru>
2025-10-14 08:56:03 +0700

    create main.cpp

---
+++ oop/2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+// #include "vector.cpp"
+
+#include "vector.tpp"
+
+int main(){
+    int mas[] = {1, 2, 3, 4, 5}, mas2[] = {6, 7, 8};
+    Vector<int> vec(mas, 5), vec2(mas2, 3);
+    Vector<short int> vecs = {0, 2, 3, 3, 3}; 
+}
