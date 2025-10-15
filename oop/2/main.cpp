42b7fe52499b224b44e838b87d9b070907190ef3
qRugma <strugov.poma@yandex.ru>
2025-10-15 16:30:34 +0700

    add tests

--- oop/2/main.cpp
+++ oop/2/main.cpp
@@ -6,7 +6,50 @@
 #include "vector.tpp"
 
 int main(){
+    int choise = 0;
     int mas[] = {1, 2, 3, 4, 5}, mas2[] = {6, 7, 8};
     Vector<int> vec(mas, 5), vec2(mas2, 3);
-    Vector<short int> vecs = {0, 2, 3, 3, 3}; 
+    Vector<short int> vecs = {0, 2, 3, 3, 3};
+    std::cout << "0 - auto, 1 - write\n";
+    std::cin >> choise;
+    if (choise){
+        std::cout << "write 5 elements: ";
+        std::cin >> vec;
+        std::cout << std::endl << "write 3 elements";
+        std::cin >> vec2;
+    }
+    std::cout << "vec.size: ";
+    std::cout << vec.size() << std::endl;
+    std::cout << "vec.swap(vec2)\n";
+    vec.swap(vec2);
+    std::cout << vec << std::endl;
+    vec.swap(vec2);
+    std::cout << "vec.insert(index: 3, value: 5)\n";
+    vec.insert(3, 5);
+    std::cout << vec << std::endl;
+    std::cout << "vec.pop(index: 3)\n";
+    vec.pop(3);
+    std::cout << vec << std::endl;
+    
+    std::cout << "vecs.remove(value: 3)\n";
+    vecs.remove(3);
+    std::cout << vecs << std::endl;
+    
+    std::cout << "vecs.remove_all(value: 3)\n";
+    vecs.remove_all(3);
+    std::cout << vecs << std::endl;
+    
+    std::cout << "vec.max(): " << vec.max() << std::endl;
+    std::cout << "vec.min(): " << vec.min() << std::endl;
+    
+    std::cout << "vec[2] = " << vec[2] << std::endl;
+    
+    std::cout << "vec += 2: ";
+    std::cout << (vec+=2) << std::endl;
+    
+    std::cout << "vec += vec2\n";
+    std::cout << (vec += vec2) << std::endl;
+
+    std::cout << "vec == vec: " << (vec == vec) << std::endl;
+    std::cout << "vec == vec2: " << (vec == vec2) << std::endl;
 }
