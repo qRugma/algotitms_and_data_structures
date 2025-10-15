c17293154dc9c111e9612aa15d121e38cda96d2e
qRugma <strugov.poma@yandex.ru>
2025-10-15 17:20:39 +0700

    add for and correct heapify

--- algorithm/3/lab3.cpp
+++ algorithm/3/lab3.cpp
@@ -6,21 +6,21 @@
 
 void heapify(int* mas, int len, int i){
     //2
-    int maxChild = (mas[2*i+1] < mas[2*i+2]) and !(2*i+2 == len) ? 2*i+2 : 2*i+1;
+    int maxChild = (mas[2*i+1] < mas[2*i+2]) and (2*i+2 < len) ? 2*i+2 : 2*i+1;
     //3
     if (mas[i] >= mas[maxChild])
         return;
     //4
     std::swap(mas[i], mas[maxChild]);
-    if(maxChild <= (len-1) / 2)
+    if(maxChild <= len/2 - 1)
         heapify(mas, len, maxChild);
 }
 
 void PiramSort(int* mas, int len){
-    const int sep = (len-1) / 2;
+    const int sep = len/2-1;
     for(int i = sep; i >= 0; i--)
         heapify(mas, len, i);
-    for(int i=len-1; i!=1; i--){
+    for(int i=len-1; i>=0; i--){
         std::swap(mas[0], mas[i]);
         heapify(mas, i, 0);
         // for(int i=0; i<len; i++)
@@ -50,7 +50,7 @@ int main(){
         std::cout << mas[i] << " ";
     std::cout << std::endl;
     std::cout << is_sorted(mas, mas + len) << std::endl;
-    std::cout << "size\td\tis_sorted\ttime\n";
+    std::cout << "size\t\td\tis_sorted\ttime\n";
     for(int size=10000; size<=1000000; size*=10)
         for(int d=10; d<=100000; d*=100){
             char buf[100];
@@ -61,13 +61,16 @@ int main(){
             for(int i=0; i<size; i++)
                 fout << (mas[i] = distribution(generator)) << " ";
             fout.close();
-            
-            std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
-            PiramSort(mas, size);
-            std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> duration = timeEnd - timeStart;
+            double sumdur = 0;
+            for(int i = 0; i<3; i++){
+                std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
+                PiramSort(mas, size);
+                std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> duration = timeEnd - timeStart;
+                sumdur += duration.count();
+            }
                 
-            std::cout << size << "\t" << d << "\t" << is_sorted(mas, mas+size) << "\t\t" << duration.count() << std::endl;
+            std::cout << size << "\t\t" << d << "\t" << is_sorted(mas, mas+size) << "\t\t" << (sumdur / 3) << std::endl;
 
             delete[] mas;
         }
