8d1a1abf30fdc42cc9d4386d0830cb4415337849
qRugma <strugov.poma@yandex.ru>
2025-10-29 19:39:39 +0700

    add test for BooleanVector(const uint32_t, const bool)

--- oop/3/main.cpp
+++ oop/3/main.cpp
@@ -33,12 +33,16 @@ int main(){
     vec.set_value_all(0);
     std::cout << vec << std::endl;
 
-    std::cout << "set_value_from(value: 1, index: 2)"<< std::endl;
-    vec.set_value_from(1, 2);
+    std::cout << "set_value_from(value: 1, index: 2, k: 3)"<< std::endl;
+    vec.set_value_from(1, 2, 3);
     std::cout << vec << std::endl;
 
 
-    BooleanVector vec0 = "0000000001", vec1="1111110011";
+    BooleanVector vec0(10, 0), vec1(10, 1);
+    std::cout << vec.getLenght() << std::endl;
+    std::cout << vec0.getLenght() << std::endl;
+    vec0.set_value(1, 9);
+    vec1.set_value_from(0,6, 2);
     std::cout << "vec0 = " << vec0 << std::endl;
     std::cout << "vec1 = " << vec1 << std::endl;
 
