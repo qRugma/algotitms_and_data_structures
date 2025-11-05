0a4c027dcb9ba84727185faaee4ce84110fbe680
qRugma <strugov.poma@yandex.ru>
2025-11-05 18:55:54 +0700

    add type arg for ShelSort, format answer

--- algorithm/4/main.cpp
+++ algorithm/4/main.cpp
@@ -18,11 +18,16 @@ int next_H3(int i){
     return 8*pow(2, i) - 6*pow(2, i/2) +1;
 }
 
-void ShelSort1(int* mas, int len){
-    int t = log2(len);
+void ShelSort(int* mas, int len, int type=0){
     int s = len;
     for(int i=log2(len); i>=0; i--){
-        s = next_H1(s);
+        if (type==0)
+            s = next_H1(s);
+        else if(type==1)
+            s = next_H2(i);
+        else if(type==2)
+            s = next_H3(i);
+        
         for(int b=0; b < s; b++){
             int j = b+s;
             while(j < len){
@@ -40,44 +45,6 @@ void ShelSort1(int* mas, int len){
     }
 }
 
-void ShelSort2(int* mas, int len){
-    for(int i=log2(len); i>=0; i--){
-        int s = next_H2(i);
-        for(int b=0; b < s; b++){
-            int j = b+s;
-            while(j < len){
-                int x = mas[j];
-                int k = j - s;
-                while(k >= 0 and mas[k] > x){
-                    mas[k+s] = mas[k];
-                    k = k-s;
-                }
-                mas[k+s] = x;
-                j += s;
-            }
-        }
-    }
-}
-
-void ShelSort3(int* mas, int len){
-    for(int i=log2(len); i>=0; i--){
-        int s = next_H3(i);
-        for(int b=0; b < s; b++){
-            int j = b+s;
-            while(j < len){
-                int x = mas[j];
-                int k = j - s;
-                while(k >= 0 and mas[k] > x){
-                    mas[k+s] = mas[k];
-                    k = k-s;
-                }
-                mas[k+s] = x;
-                j += s;
-            }
-        }
-    }
-}
-
 bool is_sorted(int* begin, int* end){
     begin++;
     for(; begin!=end; begin++)
@@ -86,41 +53,43 @@ bool is_sorted(int* begin, int* end){
     return true;
 }
 
-
 int main(){
     std::ifstream fin;
     const int len = 9;
     int mas[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    ShelSort2(mas, len);
+    ShelSort(mas, len);
     for(int i=0; i<len; i++)
         std::cout << mas[i] << " ";
     std::cout << std::endl;
     std::cout << is_sorted(mas, mas + len) << std::endl;
-    std::cout << "size\t\td\tis_sorted\ttime\n";
-    for(int size=10000; size<=1000000; size*=10)
-        for(int d=10; d<=100000; d*=100){
-            char buf[100];
-            snprintf(buf, sizeof(buf), "S%dD%d.txt", size, d);
-            fin.open(buf);
-            int *mas = new int[size];
-            for(int i=0; i<size; i++)
-                fin >> mas[i];
-            fin.close();
-            double sumdur = 0;
-            int *itermas = new int[size];
-            for(int i = 0; i<3; i++){
-                std::memcpy(itermas, mas, size*sizeof(int));
-                std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
-                ShelSort2(itermas, size);
-                std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> duration = timeEnd - timeStart;
-                sumdur += duration.count();
+    for(int type=0; type < 3; type++){
+        std::cout << "type = " << type << std::endl;
+        std::cout << "size\t\td\tis_sorted\ttime\n";
+        for(int size=10000; size<=1000000; size*=10)
+            for(int d=10; d<=100000; d*=100){
+                char buf[100];
+                snprintf(buf, sizeof(buf), "S%dD%d.txt", size, d);
+                fin.open(buf);
+                int *mas = new int[size];
+                for(int i=0; i<size; i++)
+                    fin >> mas[i];
+                    fin.close();
+                    double sumdur = 0;
+                int *itermas = new int[size];
+                for(int i = 0; i<3; i++){
+                    std::memcpy(itermas, mas, size*sizeof(int));
+                    std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
+                    ShelSort(itermas, size, type);
+                    std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> duration = timeEnd - timeStart;
+                    sumdur += duration.count();
+                    
+                }
+                    
+                std::cout << size << "\t\t" << d << "\t" << is_sorted(itermas, itermas+size) << "\t\t" << (sumdur / 3) << std::endl;
                 
+                delete[] mas;
+                delete[] itermas;
             }
-                
-            std::cout << size << "\t\t" << d << "\t" << is_sorted(itermas, itermas+size) << "\t\t" << (sumdur / 3) << std::endl;
-
-            delete[] mas;
-            delete[] itermas;
     }
 }
