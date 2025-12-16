5c9eb2e732faad270517f9b6ee92ad53be3f474a
qRugma <strugov.poma@yandex.ru>
2025-12-17 02:30:39 +0700

    formatting

--- algorithm/7/mainLink.cpp
+++ algorithm/7/mainLink.cpp
@@ -1,93 +1,82 @@
-#include "booleanvector.cpp"
+#include "booleanvector.hpp"
 #include "linkedlist.hpp"
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include <fstream>
-
 
 struct Leader;
 
-struct Trailer{
-    Leader *leaderPtr;
+struct Trailer {
+    Leader* leaderPtr;
 };
 
-struct Leader{
+struct Leader {
     int VertexNumber;
     int inDegree;
-    LinkedList<Trailer> *trailerList;
+    LinkedList<Trailer>* trailerList;
 };
 
-class VertexFinder{
-    public:
+class VertexFinder {
+  public:
+    VertexFinder(const int vertexNumber) : vertexNumber_(vertexNumber) {}
 
-        VertexFinder(const int vertexNumber) : vertexNumber_(vertexNumber) {}
+    bool operator()(const Leader& leader) { return leader.VertexNumber == vertexNumber_; }
 
-        bool operator()(const Leader& leader)
-        {
-            return leader.VertexNumber == vertexNumber_;
-        }
-
-    private:
-
-        int vertexNumber_;
+  private:
+    int vertexNumber_;
 };
 
-
-bool isEdge(LinkedList<Leader> &list, Leader &lead, BooleanVector &ignoreEdges){
+bool isEdge(LinkedList<Leader>& list, Leader& lead, BooleanVector& ignoreEdges) {
     int inDegree = lead.inDegree;
-    for (auto item : list){
-        if (!ignoreEdges[item.VertexNumber-1])
+    for (auto item : list) {
+        if (!ignoreEdges[item.VertexNumber - 1])
             continue;
         for (auto tra : *item.trailerList)
             if (tra.leaderPtr->VertexNumber == lead.VertexNumber)
-            inDegree--;
+                inDegree--;
     }
     return inDegree == 0;
 }
 
-std::vector<int> TopSortLink(LinkedList<Leader> list){
+std::vector<int> TopSortLink(LinkedList<Leader> list) {
     int maxNum = 0;
-    for (auto lead : list)
+    for (auto lead : list) 
         maxNum = std::max(maxNum, lead.VertexNumber);
 
     BooleanVector ignoreEdges(maxNum, 0);
     std::vector<int> result;
-    while(ignoreEdges.getWeight() != ignoreEdges.getLenght())
-        for(auto lead : list)
-            if(!ignoreEdges[lead.VertexNumber-1] and isEdge(list, lead, ignoreEdges)){
+    while (ignoreEdges.getWeight() != ignoreEdges.getLenght())
+        for (auto lead : list)
+            if (!ignoreEdges[lead.VertexNumber - 1] and isEdge(list, lead, ignoreEdges)) {
                 result.push_back(lead.VertexNumber);
-                ignoreEdges.set_value(1,lead.VertexNumber-1);
+                ignoreEdges.set_value(1, lead.VertexNumber - 1);
             }
     return result;
 }
 
-int main(){
+int main() {
     LinkedList<Leader> graph;
     int inVertex, outVertex;
     std::ifstream graphFile("graph.txt");
 
-    while (true){
+    while (true) {
         graphFile >> inVertex >> outVertex;
         if (inVertex == 0 and outVertex == 0)
             break;
 
         auto firstVertexIt = graph.find_if(VertexFinder(inVertex));
-        
-        if (firstVertexIt == graph.end())
-            graph.push_front(
-                Leader{inVertex, 0, new LinkedList<Trailer>()}
-            );
 
-        Leader *pA = &(*graph.find_if(
-            VertexFinder(inVertex)
-        ));
+        if (firstVertexIt == graph.end())
+            graph.push_front(Leader{inVertex, 0, new LinkedList<Trailer>()});
+
+        Leader* pA = &(*graph.find_if(VertexFinder(inVertex)));
 
         auto secondVertexIt = graph.find_if(VertexFinder(outVertex));
 
         if (secondVertexIt == graph.end())
             graph.push_front(Leader{outVertex, 0, new LinkedList<Trailer>()});
-        
-        Leader *pB = &(*graph.find_if(VertexFinder(outVertex)));
+
+        Leader* pB = &(*graph.find_if(VertexFinder(outVertex)));
 
         ++pB->inDegree;
 
@@ -95,7 +84,6 @@ int main(){
     }
 
     auto answer = TopSortLink(graph);
-    for(auto i : answer)
+    for (auto i : answer) 
         std::cout << i << " ";
-
 }
