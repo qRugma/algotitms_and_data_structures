09c9f9a0bcbc1fd9acd8a14841be592b7d1e407e
qRugma <strugov.poma@yandex.ru>
2026-05-12 09:09:02 +0700

    first good tests

--- algorithm2/1/main2.cpp
+++ algorithm2/1/main2.cpp
@@ -3,15 +3,18 @@
 #include <fstream>
 #include <cstring>
 #include <chrono>
+#include <vector>
+#include <string>
 
 #define out  std::ios_base::openmode::_S_out
 #define in   std::ios_base::openmode::_S_in
 
-void sortFile(const std::string &file_name, const size_t files_count=3){
+std::string sortFile(const std::string &file_name, const size_t files_count=3){
     std::ifstream fin(file_name);
     size_t n = files_count+1;
     int *ms = new int[n], *ip = new int[n];
     std::fstream *f_i = new std::fstream[n];
+    std::vector<std::string> f_names(n);
     size_t file_cur=0;
     size_t f_in = files_count;
     int elem_prev, elem_cur;
@@ -19,7 +22,8 @@ void sortFile(const std::string &file_name, const size_t files_count=3){
     //Первичное разбиение
     for (size_t i=0; i<n; i++){
         ms[i] = 1; ip[i] = 1;
-        f_i[i].open("./cache/f_"+ std::to_string(i), out);
+        f_names[i] = "./cache/f_"+ std::to_string(i); 
+        f_i[i].open(f_names[i], out);
     }
     ms[n-1] = 0; ip[n-1] = 0;
     while (!fin.eof()){
@@ -30,13 +34,14 @@ void sortFile(const std::string &file_name, const size_t files_count=3){
     fin.close();
     f_i[f_in].close();
     //разбиение
-    f_i[f_in].open("./cache/f_"+ std::to_string(f_in), in);
+    f_i[f_in].open(f_names[f_in], in);
     elem_prev = - (__INT_MAX__-1);
     int L = 1;
     while (!f_i[f_in].eof()){
         f_i[f_in] >> elem_cur;
         if (elem_prev > elem_cur){
             ms[file_cur]--;
+            f_i[file_cur] << "#";
             if (ms[file_cur] < ms[file_cur+1]) file_cur = (file_cur + 1) % (files_count);
             else if (ms[file_cur] == 0){
                 L++; int ip0 = ip[0]; file_cur = 0;
@@ -52,90 +57,99 @@ void sortFile(const std::string &file_name, const size_t files_count=3){
         f_i[file_cur] << " " << elem_cur;
         std::cout << elem_cur << " ";
     }
+    ms[file_cur]--;
+    f_i[file_cur] << "#";
     
     for (size_t i=0; i<n; i++){
         f_i[i].close();
         if (i == f_in)
-            f_i[f_in].open("./cache/f_"+ std::to_string(f_in), in);
+            f_i[f_in].open(f_names[f_in], out);
         else
-            f_i[i].open("./cache/f_"+ std::to_string(i), out);
+            f_i[i].open(f_names[i], in);
     }
     bool have_zero = false;
     int *elem_curs = new int [files_count]; 
+    bool *flag_curs = new bool [files_count]; 
     for (size_t i=0; i<files_count; i++){
-        if (!f_i[i].eof())  
+        if (!f_i[i].eof())
             f_i[i] >> elem_curs[i];
-    while (L > 0){
-        while (!f_i[n - 2].eof()){
-            if (ms[file_cur] == 0){
-                have_zero = true;
-
-            }
-
-        }
-            
+        flag_curs[i] = true;
     }
+    char buf;
+    int flag_count = 0;
+    std::cout << std::endl;
+    while (L > 0){
+        std::cout << "L=" << L << ":";
+        while (!f_i[n-2].eof()){
+            // определение сливаемых отрезков
+            for (size_t i=0; i<files_count; i++){
+                if (ms[i]>0){
+                    flag_curs[i] = false;
+                    ms[i]--;
+                    continue;
+                } else if (ms[i] == 0){
+                    flag_curs[i] = true;
+                    flag_count++;
+                }
+            }
+            if (flag_count == 0){
+                ms[n-1] += 1;
+                continue; 
+            }
+            // слияние отрезков в f_in
+            while (flag_count >0){
+                // поиск сливаемого элемента
+                int min = __INT_MAX__, min_index; 
+                for (size_t j=0; j<files_count; j++){
+                    if (!flag_curs[j])
+                        continue;
+                    if (min > elem_curs[j]){
+                        min = elem_curs[j];
+                        min_index = j;
+                    }
+                }
+                f_i[f_in] << " " << min; 
+                std::cout << " " << min;
+                // определение того кончился ли отрезок
+                f_i[min_index].get(buf);
+                if (buf == '#'){
+                    flag_curs[min_index] = false;
+                    flag_count--;
+                    f_i[min_index] >> elem_curs[min_index];
+                } else {
+                    f_i[min_index] >> elem_curs[min_index];
+                }
+            }
+            f_i[f_in] << "#";
+            std::cout << "#" << std::endl;
+        } 
+        L--;
+        f_i[n-2].close();
+        f_i[n-2].open(f_names[n-2], out);
+        f_i[n-1].close();
+        f_i[n-1].open(f_names[n-1], in);
+        f_i[n-1] >> elem_curs[n-1];
+        for(size_t i=files_count-1; i>0; i--){
+            std::swap(f_i[i], f_i[i-1]);
+            std::swap(f_names[i], f_names[i-1]);
+            std::swap(ms[i], ms[i-1]);
+            std::swap(ip[i], ip[i-1]);
+            std::swap(elem_curs[i], elem_curs[i-1]);
+            std::swap(flag_curs[i], flag_curs[i-1]);
+        }
+        std::swap(f_i[n-1], f_i[0]);
+        std::swap(f_names[n-1], f_names[0]);
+        std::swap(ms[n-1], ms[0]);
+        std::swap(ip[n-1], ip[0]);
+        std::swap(elem_curs[n-1], elem_curs[0]);
+        std::swap(flag_curs[n-1], flag_curs[0]);
 
-    //Цикл слияний
-    // int *elem_curs = new int[files_count], phase = 0;
-    // bool *files_flags = new bool[files_count];
-    // std::fstream *files_in, *files_out;
-    // while(true){
-    //     bool end_sort = true;
-    //     if (phase % 2 == 0)
-    //         for (size_t i=0; i<files_count; i++){
-    //             f_i[i].open("./cache/f_"+ std::to_string(i), in);
-    //             files_in = f_i;
-    //             g_i[i].open("./cache/g_"+ std::to_string(i), out);
-    //             files_out = g_i;
-    //         }
-    //     else
-    //         for (size_t i=0; i<files_count; i++){
-    //             f_i[i].open("./cache/f_"+ std::to_string(i), out);
-    //             files_out = f_i;
-    //             g_i[i].open("./cache/g_"+ std::to_string(i), in);
-    //             files_in = g_i;
-    //         }
+    }
+    std::cout << std::endl << "результат в " << f_names[0] << std::endl;
+    for (size_t i=0; i<files_count; i++)
+        f_i[i].close();
+    return f_names[0];
 
-    //     for (size_t i=0; i<files_count; i++) files_flags[i] = true;
-        
-    //     for (size_t i=0; i<files_count; i++){
-    //         if (!files_in[i].eof())
-    //             files_in[i] >> elem_curs[i];
-        // }
-
-    //     //слияние
-    //     for (size_t i=0; i<files_count; i++){
-    //         int prev=-__INT_MAX__+1;
-            
-    //         while(true){
-    //             bool next_file=true;
-    //             int min=__INT_MAX__, min_index=-1;
-    //             for (size_t i=0; i<files_count; i++)
-    //                 if (files_flags[i] and (elem_curs[i] < min) and (elem_curs[i] >= prev)){
-    //                     min=elem_curs[i];
-    //                     min_index = i;
-    //                 }
-    //             if (min_index == -1)
-    //                 break;
-    //             if (!files_in[min_index].eof())
-    //                 files_in[min_index] >> elem_curs[min_index];
-    //             else
-    //                 files_flags[min_index] = false;
-    //             prev = min;
-    //             files_out[i] << " " << min;
-    //             if (i>0)
-    //                 end_sort = false;
-    //         }
-    //     }
-    //     for (size_t i=0; i<files_count; i++){
-    //         f_i[i].close();
-    //         g_i[i].close();
-    //     }
-    //     if (end_sort)
-    //     break;
-    //     phase++;
-    // }
 }
 
 
@@ -155,7 +169,7 @@ int createAndSortFile(const std::string &file_name, const int numbersCount, cons
     if (!isFileContainsSortedArray(file_name)) {
         return -2;
     }
-
+    std::cout << "Файл отсортирован!" << std::endl;
     return 1;
 }
 
